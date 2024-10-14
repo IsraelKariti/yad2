bcc15173bd2bafc39977aa5e0ff8af266d8f8556
israelkariti <israelkariti@gmail.com>
2024-10-14 20:20:33 +0300

    add leading categories to home page

--- src/components/homepage/LeadingCategories/LeadingCategoriesContainer.js
+++ src/components/homepage/LeadingCategories/LeadingCategoriesContainer.js
@@ -4,12 +4,13 @@ import '../../../styles/leadingCategoriesContainer.scss'
 
 export const LeadingCategoriesContainer = ()=>{
     return <div className="leading-categories-container">
-        <LeadingCategory/>
-        <LeadingCategory/>
-        <LeadingCategory/>
-        <LeadingCategory/>
-        <LeadingCategory/>
-        <LeadingCategory/>
-        <LeadingCategory/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-business.webp" title="עסקים למכירה"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-professional.webp" title="בעלי מקצוע"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-pets.webp" title="חיות מחמד"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-drushim-new.webp" title="דרושים"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-second-hand.webp" title="יד שנייה"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-vehicles.webp" title="רכב"/>
+        <LeadingCategory imgSrc="/assets/Yad2Categories/leading-category-real-estate.webp" title='נדל"ן'/>
+
     </div>
 }
--- src/components/homepage/LeadingCategories/LeadingCategory.js
+++ src/components/homepage/LeadingCategories/LeadingCategory.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const LeadingCategory = ()=>{
-    return <div className='leading-category'>category</div>
+export const LeadingCategory = ({imgSrc, title})=>{
+    return <div className='leading-category'>
+            <div className='leading-category-image-container'>
+                <img className='leading-category-image' src={imgSrc} alt="the category"/>
+            </div>
+            <p className='leading-category-title'>{title}</p>
+        </div>
 }
