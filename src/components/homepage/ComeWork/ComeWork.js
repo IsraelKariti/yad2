8ac01816f4298d1634b1149165d0a9e9a3cd0de4
israelkariti <israelkariti@gmail.com>
2024-11-03 10:05:22 +0200

    come work with us

---
+++ src/components/homepage/ComeWork/ComeWork.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../../styles/ComeWork.style.scss';
+
+export const ComeWork = ()=>{
+    return <div className='comeWork'>
+        <img className='comeWorkNarrow' alt="come work with us" src="/assets/comeWorkWithUs/comework_narrow.webp"/>
+        <img className='comeWorkWide' alt="come work with us" src="/assets/comeWorkWithUs/comework_wide.webp"/>
+    </div>
+}
--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,11 +1,13 @@
 import { Carousel } from "./Carousel/Carousel"
 import { LeadingCategoriesContainer } from "./LeadingCategories/LeadingCategoriesContainer"
 import { NewInYourArea } from "./NewInYourArea/NewInYourArea";
+import { ComeWork } from "./ComeWork/ComeWork";
 
 export const HomePage = ()=>{
     return <div>
         <Carousel/> 
         <LeadingCategoriesContainer/>
         <NewInYourArea/>
+        <ComeWork/>
     </div>
 }
